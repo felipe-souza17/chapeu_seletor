2e0a2565b45191014599b62ec20a7884de054558
felipe-souza17 <felipesantos.rsouza@gmail.com>
2022-04-28 11:41:12 -0300

    Refatoração do código para verificar a casa

--- src/app/chapeu/chapeu.page.ts
+++ src/app/chapeu/chapeu.page.ts
@@ -13,32 +13,29 @@ export class ChapeuPage implements OnInit {
   imageSrc: string;
   text: string;
   subtext: string;
+  result: string;
 
   constructor(public dadosService: DadosService) { }
 
   ngOnInit() {
     this.username = this.dadosService.pegarDados('username');
     this.imageSrc = '../../assets/img/witch_hat.png';
-    this.text = 'Clique no chapéu para verificar qual é sua casa.';
+    this.text = 'Certo, agora veremos de que casa você será!';
+    this.subtext = 'Clique no chapéu para verificar qual é sua casa.';
+    this.result = '';
   }
 
   checkHouse() {
     const randomNumber = Math.floor(Math.random() * 4);
+    const housesImage = ['../../assets/img/leao.png', '../../assets/img/cobra.png', '../../assets/img/gamba.png', '../../assets/img/aguia.png'];
     const house = randomNumber === 0 ? 'Grifinória' : (randomNumber === 1 ? 'Sonserina' : (randomNumber === 2 ? 'Lufa-Lufa' : 'Corvinal' ));
 
     if(!this.clicked) {
       this.clicked = true;
-      if (randomNumber === 0) {
-        this.imageSrc = '../../assets/img/leao.png';
-      } else if (randomNumber === 1 ) {
-        this.imageSrc = '../../assets/img/cobra.png';
-      } else if (randomNumber === 2 ) {
-        this.imageSrc = '../../assets/img/gamba.png';
-      } else {
-        this.imageSrc = '../../assets/img/aguia.png';
-      }
-      this.text = '';
-      this.subtext = `Parabéns, ${this.username}. Você acaba de ser juntar a casa ${house}!`;
+      this.imageSrc = housesImage[randomNumber];
+      this.text = `Parabéns, ${this.username}!`;
+      this.subtext = '';
+      this.result = `Você acaba de se juntar a casa ${house}!`;
     }
 
 
